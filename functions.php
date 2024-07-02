e92c84993f7f785b4314b41c17e490527808ebe5
Wesley Cardoso <cardososads@github.com>
2024-07-02 01:34:25 -0400

    Forms conversation 02

--- functions.php
+++ functions.php
@@ -37,28 +37,33 @@ add_action( 'wp_enqueue_scripts', 'hello_elementor_child_scripts_styles', 20 );
 
 require_once get_template_directory() . 'classes/class-numerology-calculator.php';
 
-function process_form_data() {
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+function calculate_destiny_number() {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['first_name']) && isset($_POST['birth_date'])) {
         $calculator = new NumerologyCalculator();
-
-        // Verifique qual formulário foi enviado e colete os dados apropriados
-        if (isset($_POST['first_name']) && isset($_POST['birth_date'])) {
-            $firstName = sanitize_text_field($_POST['first_name']);
-            $birthDate = sanitize_text_field($_POST['birth_date']);
-            $destinyNumber = $calculator->calculateDestinyNumber($firstName, $birthDate);
-            // Salvar ou redirecionar com o resultado
-        } elseif (isset($_POST['full_name'])) {
-            $fullName = sanitize_text_field($_POST['full_name']);
-            $expressionNumber = $calculator->calculateExpressionNumber($fullName);
-            // Salvar ou redirecionar com o resultado
-        } elseif (isset($_POST['email']) && isset($_POST['marital_status'])) {
-            $fullName = sanitize_text_field($_POST['full_name']);
-            $email = sanitize_email($_POST['email']);
-            $maritalStatus = sanitize_text_field($_POST['marital_status']);
-            $motivationNumber = $calculator->calculateMotivationNumber($fullName);
-            // Salvar ou redirecionar com o resultado
-        }
+        $firstName = sanitize_text_field($_POST['first_name']);
+        $birthDate = sanitize_text_field($_POST['birth_date']);
+        $destinyNumber = $calculator->calculateDestinyNumber($firstName, $birthDate);
+        var_dump($destinyNumber); // Exibe o resultado
     }
 }
+add_shortcode('calculate_destiny_number', 'calculate_destiny_number');
 
-add_shortcode('process_form', 'process_form_data');
+function calculate_expression_number() {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['full_name'])) {
+        $calculator = new NumerologyCalculator();
+        $fullName = sanitize_text_field($_POST['full_name']);
+        $expressionNumber = $calculator->calculateExpressionNumber($fullName);
+        var_dump($expressionNumber); // Exibe o resultado
+    }
+}
+add_shortcode('calculate_expression_number', 'calculate_expression_number');
+
+function calculate_motivation_number() {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['full_name'])) {
+        $calculator = new NumerologyCalculator();
+        $fullName = sanitize_text_field($_POST['full_name']);
+        $motivationNumber = $calculator->calculateMotivationNumber($fullName);
+        var_dump($motivationNumber); // Exibe o resultado
+    }
+}
+add_shortcode('calculate_motivation_number', 'calculate_motivation_number');
